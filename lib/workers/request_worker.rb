af6c0823a3d3828c77edae8dc3ef4fdf8ff4dfea
Jonathan Kirst <jskirst@gmail.com>
2017-05-04 20:46:39 +0000

    Added request worker

---
+++ lib/workers/request_worker.rb
@@ -0,0 +1,12 @@
+require "net/http"
+require "uri"
+
+module Workers
+  class GetRequestWorker < BaseWorker
+    def perform(url)
+      uri = URI.parse(url)
+      response = Net::HTTP.get_response(uri)
+      puts "Done! Called #{url} and got #{response.code}"
+    end
+  end
+end
