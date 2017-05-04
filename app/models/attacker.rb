363c8dde7816f1528b2fdd4d081b5aded039d4e7
Jonathan Kirst <jskirst@gmail.com>
2017-05-04 23:33:54 +0000

    Adding keys

--- app/models/attacker.rb
+++ app/models/attacker.rb
@@ -1,7 +1,7 @@
 class Attacker < ActiveRecord::Base
   belongs_to :siege
 
-  attr_encrypted :password
+  attr_encrypted :password, key: '3849fdd808defd9bc8d70a56691377c961b44938d14bd7272064d7800fda04b5'
 
   validates_presence_of :username, :password, :login_url
 end
