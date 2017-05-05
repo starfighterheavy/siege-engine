69e6b87ee6e8f1237569f3f3de24e189a641db5d
Jonathan Kirst <jskirst@gmail.com>
2017-05-05 22:35:46 +0000

    fixing report worker

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -4,10 +4,10 @@ require 'report'
 class ReportWorker < BaseWorker
   def perform(report_id)
     @report = Report.find(report_id)
-    @siege = @report.siege
+    @volley = @report.volley
     @report.update_attribute(:status, 'started')
     @report.content = %w[id target_id target_url target_method code time created_at].to_csv
-    @siege.results.find_each do |result|
+    @volley.results.find_each do |result|
       target = result.target
       @report.content += [result.id, target.id, target.url, target.method, result.code, result.time, result.created_at].to_csv
     end
