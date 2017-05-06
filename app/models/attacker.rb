e9e0f160815164374cf492d0222ebd3a6130c44e
Jonathan Kirst <jskirst@gmail.com>
2017-05-06 16:22:10 +0000

    Removing dead field

--- app/models/attacker.rb
+++ app/models/attacker.rb
@@ -4,8 +4,6 @@ class Attacker < ActiveRecord::Base
 
   attr_encrypted :password, key: '3849fdd808defd9bc8d70a56691377c961b44938d14bd7272064d7800fda04b5'
 
-  validates_presence_of :username, :password, :login_url
-
   def to_h
     {
       id: id,
