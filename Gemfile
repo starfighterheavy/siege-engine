b0e0ef8d13f1bfc368fb343f0a36dd9b710d0e05
Jonathan Kirst <jskirst@gmail.com>
2017-05-16 01:48:32 +0000

    Adding webmock option

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'capybara', '~> 2.13.0'
   gem 'pry'
+  gem 'webmock'
 end
 
 group :development do
---
+++ config/initializers/webmock.rb
@@ -0,0 +1,11 @@
+if ENV['WEBMOCK'] == 'true'
+  require 'webmock'
+  include WebMock::API
+
+  WebMock.enable!
+  html = "<html><head><meta name='crsf_token' value='abc'></head><body></body></html>"
+  headers = { 'set-cookie' => 'acme_session=__abc' }
+  stub_request(:any, /.*/).to_return(body: html, headers: headers)
+end
+
+
