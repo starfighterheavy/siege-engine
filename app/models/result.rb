955527e0f9c6bfb9de54caa5a4cd3afe7d7f6012
Jonathan Kirst <jc@Jonathans-MacBook-Pro.local>
2017-05-17 17:28:41 -0700

    Always including targets to decrease SQL calls

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,6 +2,8 @@ class Result < ActiveRecord::Base
   belongs_to :volley
   belongs_to :target
 
+  default_scope { includes(:target) }
+
   after_commit do
     volley.with_lock do
       volley.done! if volley.results.count == volley.strikes
