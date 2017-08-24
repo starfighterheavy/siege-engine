1f1ffeeccf656c88305e468924522d612934ff0d
Jonathan Kirst <jskirst@gmail.com>
2017-08-24 02:13:12 +0000

    Adding result body to report contents

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -4,10 +4,10 @@ require 'report'
 class ReportWorker < BaseWorker
   def run
     report.update_attribute(:status, 'started')
-    report.content = %w[id target_id target_url target_method code time created_at].to_csv
+    report.content = %w[id target_id target_url target_method code body time created_at].to_csv
     volley.results.find_each do |result|
       target = result.target
-      report.content += [result.id, target.id, target.url, target.method, result.code, result.time, result.created_at].to_csv
+      report.content += [result.id, target.id, target.url, target.method, result.code, result.body, result.time, result.created_at].to_csv
     end
     report.status = 'done'
     report.save!
